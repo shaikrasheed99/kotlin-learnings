a080a5348c85c1537acdb5f84660809fcf52e20d
rasheed <rasheed.shaik@thoughtworks.com>
2022-04-24 21:56:50 +0530

    Refactor introduce before each test method in triplet test class

--- src/test/kotlin/com/learnings/list/TripletTest.kt
+++ src/test/kotlin/com/learnings/list/TripletTest.kt
@@ -1,15 +1,21 @@
 package com.learnings.list
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class TripletTest {
+
+    private lateinit var triplet: Triplet
+
+    @BeforeEach
+    internal fun setUpTriplet() {
+        triplet = Triplet()
+        triplet.list = listOf(3, 4, 5)
+    }
+
     @Test
     internal fun shouldHaveListOfThreeNumberInsideTriplet() {
-        val triplet = Triplet()
-
-        triplet.list = listOf(3, 4, 5)
-
         assertEquals(3, triplet.list.size)
     }
 }
