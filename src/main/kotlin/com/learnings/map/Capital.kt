b77acc4ba0a056141566d67e1c710271a2bf4411
rasheed <rasheed.shaik@thoughtworks.com>
2022-04-24 22:15:56 +0530

    Add logic to insert delhi as a capital to india inside countries map

---
+++ src/main/kotlin/com/learnings/map/Capital.kt
@@ -0,0 +1,5 @@
+package com.learnings.map
+
+class Capital {
+    var map: HashMap<String, String> = HashMap()
+}
---
+++ src/test/kotlin/com/learnings/map/CapitalTest.kt
@@ -0,0 +1,21 @@
+package com.learnings.map
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class CapitalTest {
+    private lateinit var capital: Capital
+
+    @BeforeEach
+    internal fun setUpCapital() {
+        capital = Capital()
+    }
+
+    @Test
+    internal fun shouldHaveDelhiAsCapitalOfIndia() {
+        capital.map["India"] = "Delhi"
+
+        assertEquals("Delhi", capital.map["India"])
+    }
+}
